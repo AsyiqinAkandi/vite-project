e28e31a4c9fd88be4f334aee25a4caeecca1f25d
AsyiqinAkandi <20FTT1461@student.pb.edu.bn>
2022-04-16 07:43:19 +0800

    profile info edit

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,25 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+// <script setup>
+//   console.log('hello script setup')
+// </script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+  <div id="app">
+      <img src="./img/Scan Passport Photo.JPG" alt="Not available">
+      <br><h3>Khairunnisa' Asyiqin binti Akandi</h3>
+      <div id="info">
+        <p>D.O.B: 14/01/2000</p><br>
+        <p>School: School of Information and Technology</p><br>
+        <p>Institution: Politeknik Brunei</p><br>
+        <p>Course: Diploma in Web Development</p>
+      </div>
+    </div>
+
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+
 </template>
 
 <style>
